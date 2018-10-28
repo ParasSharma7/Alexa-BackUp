e29535ef0dc367f427e306c02f86b1d4a18b4a8e
Paras Sharma <33489585+ParasSharma7@users.noreply.github.com>
2018-10-28 14:35:15 +0530

    Update backup.py

--- backup.py
+++ backup.py
@@ -2,13 +2,19 @@ from aws_backup import *
 from main_window import *
 
 if __name__ == '__main__':
-	if !(os.path.isfile('user')):
-		app = QApplication(sys.argv)
-		ex = Backup()
-		sys.exit(app.exec_())
-		var_list = ex.pass1
-		#assert 1<0
-		add_update_new_device()
-	while(True):
-		update_device()
-		time.sleep(60)
+    if not (os.path.isfile('user')):
+        while(True):
+            app = QApplication(sys.argv)
+            ex = Backup()
+            app.exec_()
+            #sys.exit(app.exec_())
+            var_list = ex.getpass()
+            print(var_list)
+            
+            print("yes")
+            #sys.exit(app.exec_())
+            if(len(var_list)==4): 
+                add_update_new_device(var_list)
+                while(True):
+                    update_device()
+                    time.sleep(60)
