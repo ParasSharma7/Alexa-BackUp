567f69f969854a7a41c62ba795f13859ffceb871
divyam02 <41248177+divyam02@users.noreply.github.com>
2018-11-04 01:15:36 +0530

    Added script restart, sync resume etc.

--- backup.py
+++ backup.py
@@ -1,9 +1,20 @@
 import time
+import os
 from aws_backup import *
 from main_window import *
+import getpass
+USER_NAME = getpass.getuser()
+
+def add_to_startup():
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    with open(bat_path + '\\' + "BackMeUpOnStartup.bat", "w+") as bat_file:
+        bat_file.write('cd '+file_path+'\n')
+        bat_file.write('python backup.py')
 
 if __name__ == '__main__':
     if not (os.path.exists('user')):
+        add_to_startup()
         while(True):
             app = QApplication(sys.argv)
             ex = Backup()
@@ -12,9 +23,17 @@ if __name__ == '__main__':
             if(len(var_list)==4): 
                 add_update_new_device(var_list)
                 while(True):
-                    update_device()
-                    time.sleep(20)
+                    try:
+                        update_device()
+                        time.sleep(20)
+                    except:
+                        print('Unable to connect to the Internet. Retrying...')
+                        time.sleep(20)
     else:
         while(True):
-            update_device()
-            time.sleep(20)
+            try:
+                update_device()
+                time.sleep(20)
+            except:
+                print('Unable to connect to the Internet. Retrying...')
+                time.sleep(20)
